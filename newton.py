b5210c77b42f21a982be4bff5109830f66058785
Brileigh Cates <brileigh_cates@berkeley.edu>
2026-08-19 14:09:10 -0700

    Added proper documentation, comments, and formatting to Newton's Method.

--- newton.py
+++ newton.py
@@ -1,40 +1,58 @@
 import numpy as np
 
+
 def first_derivative(f, x, h=1e-5):
-    return (f(x+h) - f(x))/h
+    """
+    Returns the first derivative of a function f at a value x.
+    """
+    # f'(x) = f(x+h) - f(x) / h
+    return (f(x + h) - f(x)) / h
+
 
 def second_derivative(f, x, h=1e-5):
-    
-    second = (first_derivative(f, x + h) - first_derivative(f, x))/h
-    
+    """
+    Returns the second derivative of a function f at a value x.
+    """
+    # f''(x) = f'(x+h) - f'(x) / h
+    second = (first_derivative(f, x + h) - first_derivative(f, x)) / h
     return second
 
-def optimize(f,x0,tol=1e-5):
+
+def optimize(f, x0, tol=1e-5):
+    """
+    Function that implements Newton's method for optimization.
+    """
     x = x0
-    
-    diff = float('inf')
-    
-    while diff > tol:
-        
+
+    diff = float("inf")
+
+    while diff > tol:  # Stopping criterion for Newton's Method.
         first = first_derivative(f, x)
         second = second_derivative(f, x)
-        
-        if second == 0:
+
+        if second == 0:  # Edge-case check to avoid division by 0.
             continue
-            
+
         frac = float(first / second)
-        
+
+        # Newton's iterative function, where frac defined above is f'(x)/f''(x).
+        # x_{t+1} = x_t - f'(x_t) / f''(x_t)
         temp = x - frac
-        
+
         diff = abs(temp - x)
-        
+
         x = temp
-        
+
     return x
 
+
 def main():
-    est = optimize(np.cos,2.5)
+    """
+    Completes Newton's Method optimization with function and starting point of choice.
+    """
+    est = optimize(np.cos, 2.5)
     print(est)
+
 
 if __name__ == "__main__":
     main()
